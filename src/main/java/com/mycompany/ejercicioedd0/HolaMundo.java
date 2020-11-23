1e52b3bfc6ba8e701e9ef3861665affd92dd7a89
OscarLlaPar <74042618+OscarLlaPar@users.noreply.github.com>
2020-11-23 11:58:08 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/ejercicioedd0/HolaMundo.java
@@ -0,0 +1,12 @@
+package com.mycompany.ejercicioedd0;
+
+/**
+ * 
+ * @author Oscar Llamas Parra
+ */
+
+public class HolaMundo {
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+    }
+}
